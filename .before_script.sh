3313cda5b5c8a531ec6e59168e87a51ca49fe8c4
Paráda József <parada.jozsef@ingatlan.com>
2017-03-27 15:41:44 +0200

    CNT-987 - Travis CI: imagick telepites script futasa elott.

--- .before_script.sh
+++ .before_script.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 echo -e "\e[4mSetting up PHP Extensions for $TRAVIS_PHP_VERSION\e[0m"
-
-if [[ "$TRAVIS_PHP_VERSION" =~ "hhvm" ]] || [[ "$TRAVIS_PHP_VERSION" =~ "7." ]]; then
-    echo "Skipping"
-    exit 0
-fi
-
 printf "\n" | pecl install imagick-3.3.0
 echo "extension=imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
