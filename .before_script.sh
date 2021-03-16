92239855de61adccdee962c9273086af20a74f20
Hajdu Robert <hajdu.robert@ingatlan.com>
2021-03-16 12:51:41 +0100

    SIM-2912 - php support from 5.6 to 7.4

--- .before_script.sh
+++ .before_script.sh
@@ -2,10 +2,12 @@
 
 echo -e "\e[4mSetting up PHP Extensions for $TRAVIS_PHP_VERSION\e[0m"
 
-if [[ "$TRAVIS_PHP_VERSION" =~ "hhvm" ]] || [[ "$TRAVIS_PHP_VERSION" =~ "7." ]]; then
-    echo "Skipping"
-    exit 0
+if [[ "$TRAVIS_PHP_VERSION" =~ "5." ]]; then
+    printf "\n" | pecl install imagick-3.3.0
 fi
 
-printf "\n" | pecl install imagick-3.3.0
+if [[ "$TRAVIS_PHP_VERSION" =~ "7." ]]; then
+    printf "\n" | pecl install imagick-3.4.4
+fi
+
 echo "extension=imagick.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
